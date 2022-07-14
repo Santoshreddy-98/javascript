3ef4cee36b5c5a9587169ae2f52a0ed022e29957
Santoshreddy-98 <gorantlasantosh1998@gmail.com>
2022-07-14 19:28:25 +0530

    assignment

---
+++ onclick events/events.js
@@ -0,0 +1,24 @@
+let one=()=>{
+    let tag_Ref=document.getElementById("x")
+        tag_Ref.style.backgroundColor='brown'
+}
+let two=()=>{
+    let tag_Ref=document.getElementById("y")
+        tag_Ref.style.backgroundColor='purple'
+}
+let three=()=>{
+    let tag_Ref=document.getElementById("z")
+        tag_Ref.style.backgroundColor='indigo'
+}
+let four=()=>{
+    let tag_Ref=document.getElementById("a")
+        tag_Ref.style.backgroundColor='orange'
+}
+let five=()=>{
+    let tag_Ref=document.getElementById("b")
+        tag_Ref.style.backgroundColor='green'
+}
+let six=()=>{
+    let tag_Ref=document.getElementById("c")
+        tag_Ref.style.backgroundColor='palevioletred'
+}
