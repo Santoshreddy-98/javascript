840560b439d93fd85c5d20f56e7bd9c597faca79
Santoshreddy-98 <106945442+Santoshreddy-98@users.noreply.github.com>
2022-07-21 20:18:36 +0530

    Add files via upload

---
+++ set.js
@@ -0,0 +1,26 @@
+class person{
+    set_Name(name) {
+        this.f_Name = name
+    }
+    get_Name() {
+        return this.f_Name
+    }
+}
+
+let e1 = new person()
+e1.set_Name("Rahul Gandhi")
+console.log(e1)
+// console.log(e1.get_Name())
+let e2 = new person()
+e2.set_Name("Sonia")
+console.log(e2)
+//console.log(e2.get_Name())
+let e3 = new person()
+e3.set_Name("santosh")
+console.log(e3)
+let e4 = new person()
+e4.set_Name("gopi")
+console.log(e4)
+let e5 = new person()
+e5.set_Name("vijay")
+console.log(e5)
